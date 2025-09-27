117fcbd35011a3acf529e5c3b3d8f8d4835d1d49
Akshatkant101 <akshatkant12@gmail.com>
2025-09-28 00:22:51 +0530

    Wrapped up front-end for now

--- front-end/src/app/api/auth/[...nextauth]/options.ts
+++ front-end/src/app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,41 @@
-import { AuthOptions } from "next-auth";
+import { AuthOptions, ISODateString } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
+
+export interface CustomSession {
+  user?: CustomUser;
+  expires: ISODateString;
+}
+
+export interface CustomUser {
+  id?: string;
+  name?: string | null;
+  email?: string | null;
+  image?: string | null;
+  provider?: string | null;
+  token?: string | null;
+}
 
 export const authOption: AuthOptions = {
   pages: {
     signIn: "/",
+  },
+  callbacks: {
+    async signIn({ user, account}) {
+      console.log("The user data is:",user)
+      console.log("The account data is:",account)
+      return true
+    },
+    async session({session, user, token,}: { session: CustomSession;user: CustomUser; token: JWT; }) {
+      session.user = token.user as CustomUser;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
   },
   providers: [
     GoogleProvider({
--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,28 +1,24 @@
-import FeatureSection from '@/components/base/FeatureSection'
-import Footer from '@/components/base/Footer'
-import HeroSection from '@/components/base/HeroSection'
-import Navbar from '@/components/base/Navbar'
-import UserReviews from '@/components/base/UserReviews'
-import React from 'react'
+import FeatureSection from "@/components/base/FeatureSection";
+import Footer from "@/components/base/Footer";
+import HeroSection from "@/components/base/HeroSection";
+import Navbar from "@/components/base/Navbar";
+import UserReviews from "@/components/base/UserReviews";
+import { getServerSession } from "next-auth";
+import React from "react";
+import { authOption, CustomSession } from "./api/auth/[...nextauth]/options";
 
-const page = () => {
+const page = async () => {
+  const session: CustomSession | null = await getServerSession(authOption);
+
   return (
-     <div className="min-h-screen flex flex-col ">
-      {/* Header */}
-      <Navbar/>
-      {/* Hero Section */}
+    <div className="min-h-screen flex flex-col ">
+      <Navbar user={session?.user} />
       <HeroSection />
-
-      {/* Features Section */}
       <FeatureSection />
-
-      {/* User Reviews Section */}
       <UserReviews />
-
-      {/* Footer */}
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
