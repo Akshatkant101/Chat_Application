25aa99c400357cea951c508176291d736234a6a8
Akshatkant101 <akshatkant12@gmail.com>
2025-09-29 13:21:05 +0530

    Login logout functionality applied

---
+++ front-end/src/components/auth/ProfileMenu.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { Suspense, useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import UserAvatar from "../common/UserAvatar";
+import dynamic from "next/dynamic";
+const LogoutModal = dynamic(() => import("../auth/LogoutModal"));
+
+const ProfileMenu = ({ name, image }: { name: string; image?: string }) => {
+  const [logOutopen, setLogOutopen] = useState(false);
+
+  return (
+    <>
+      {logOutopen && (
+        <Suspense fallback={<p>Loading...</p>}>
+          <LogoutModal open={logOutopen} setOpen={setLogOutopen} />
+        </Suspense>
+      )}
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <UserAvatar name={name} image={image} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+
+          <DropdownMenuItem onClick={()=>setLogOutopen(true)}>Log Out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export default ProfileMenu;
